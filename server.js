2004d2cee7fb11e7b2f9259d2aaf796995c19b29
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-06-12 15:50:43 +0200

    Nav erin
    Heb de 2 kopjes van de nav toegevoegd en de head bug verholpen: <%- include('./partials/head.ejs') %>
    Miste de . voor de /partials

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from "express";
 
 // de basis url van de api
-const url = "https://docs.google.com/spreadsheets/d/1iFTrwoSoFcn8Jwm-MX63H3bw-nFHBzIzYn2Ygz7INYo/edit#gid=722704947";
+const url =
+  "https://docs.google.com/spreadsheets/d/1iFTrwoSoFcn8Jwm-MX63H3bw-nFHBzIzYn2Ygz7INYo/edit#gid=722704947";
 
 // maak een nieuwe express app
 const server = express();
@@ -17,7 +18,7 @@ server.set("views", "./views");
 server.use(express.static("public"));
 
 server.get("/", (request, response) => {
-    response.render("index");
+  response.render("index");
 });
 
 // Maak een route voor de sign-up pagina
@@ -27,17 +28,25 @@ server.get("/", (request, response) => {
 
 // Route voor de overview pagina
 server.get("/overview", function (req, res) {
-    res.render("overview");
+  res.render("overview");
+});
+
+server.get("/planning", function (req, res) {
+  res.render("plannning");
+});
+
+server.get("/sign-up", function (req, res) {
+  res.render("sign-up");
 });
 
 // definieer de fetchJson functie
 async function fetchJson(url) {
-    return await fetch(url)
-        .then((response) => response.json())
-        .catch((error) => error);
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
 }
 
 // Start met luisteren
 server.listen(server.get("port"), () => {
-    console.log(`Application started on http://localhost:${server.get("port")}`);
+  console.log(`Application started on http://localhost:${server.get("port")}`);
 });
