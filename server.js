411368f0ca54bc7c34dd697c6db7d9040a0c4b19
Amberhva <amber.van.der.reijden@hva.nl>
2023-06-14 12:16:50 +0200

    Fonts & custom properties
    Added the font to the project and custom properties for the colours.

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 import express from "express";
 
 // de basis url van de api
-const url =
-  "https://docs.google.com/spreadsheets/d/1iFTrwoSoFcn8Jwm-MX63H3bw-nFHBzIzYn2Ygz7INYo/edit#gid=722704947";
+const url = "https://docs.google.com/spreadsheets/d/1iFTrwoSoFcn8Jwm-MX63H3bw-nFHBzIzYn2Ygz7INYo/edit#gid=722704947";
 
 // maak een nieuwe express app
 const server = express();
@@ -17,36 +16,34 @@ server.set("views", "./views");
 // Stel de public map in
 server.use(express.static("public"));
 
+// Route voor de index pagina
 server.get("/", (request, response) => {
-  response.render("index");
+    response.render("index");
 });
-
-// Maak een route voor de sign-up pagina
-// server.get("/sign-up", function (req, res) {
-//     res.render("sign-up");
-// });
 
 // Route voor de overview pagina
 server.get("/overview", function (req, res) {
-  res.render("overview");
+    res.render("overview");
 });
 
+// Route voor de planning
 server.get("/planning", function (req, res) {
-  res.render("plannning");
+    res.render("plannning");
 });
 
+// Route voor de sign up
 server.get("/sign-up", function (req, res) {
-  res.render("sign-up");
+    res.render("sign-up");
 });
 
-// definieer de fetchJson functie
+// Definieer de fetchJson functie
 async function fetchJson(url) {
-  return await fetch(url)
-    .then((response) => response.json())
-    .catch((error) => error);
+    return await fetch(url)
+        .then((response) => response.json())
+        .catch((error) => error);
 }
 
 // Start met luisteren
 server.listen(server.get("port"), () => {
-  console.log(`Application started on http://localhost:${server.get("port")}`);
+    console.log(`Application started on http://localhost:${server.get("port")}`);
 });
