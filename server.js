e21b4313c2ef94ab801766592f95476e9b124f45
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-06-15 14:41:59 +0200

    dag week maandplanning aangepast

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from "express";
 
 // de basis url van de api
-const url = "https://docs.google.com/spreadsheets/d/1iFTrwoSoFcn8Jwm-MX63H3bw-nFHBzIzYn2Ygz7INYo/edit#gid=722704947";
+const url =
+  "https://docs.google.com/spreadsheets/d/1iFTrwoSoFcn8Jwm-MX63H3bw-nFHBzIzYn2Ygz7INYo/edit#gid=722704947";
 
 // maak een nieuwe express app
 const server = express();
@@ -18,32 +19,40 @@ server.use(express.static("public"));
 
 // Route voor de index pagina
 server.get("/", (request, response) => {
-    response.render("index");
+  response.render("index");
 });
 
 // Route voor de overview pagina
 server.get("/overview", function (req, res) {
-    res.render("overview");
+  res.render("overview");
 });
 
 // Route voor de planning
-server.get("/planning", function (req, res) {
-    res.render("plannning");
+server.get("/dagplanning", function (req, res) {
+  res.render("dagplannning");
+});
+
+server.get("/weekplanning", function (req, res) {
+  res.render("weekplannning");
+});
+
+server.get("/maandplanning", function (req, res) {
+  res.render("maandplannning");
 });
 
 // Route voor de sign up
 server.get("/sign-up", function (req, res) {
-    res.render("sign-up");
+  res.render("sign-up");
 });
 
 // Definieer de fetchJson functie
 async function fetchJson(url) {
-    return await fetch(url)
-        .then((response) => response.json())
-        .catch((error) => error);
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
 }
 
 // Start met luisteren
 server.listen(server.get("port"), () => {
-    console.log(`Application started on http://localhost:${server.get("port")}`);
+  console.log(`Application started on http://localhost:${server.get("port")}`);
 });
