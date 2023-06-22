4d39b981f0c2ccdfc8907de07afbe11a6ae72d55
Amberhva <amber.van.der.reijden@hva.nl>
2023-06-22 10:22:23 +0200

    Update
    Fix

--- public/sign-up.js
+++ public/sign-up.js
@@ -1,8 +1,41 @@
 const btn = document.getElementsById("btn");
 btn.addEventListener("click", (e) => {
-  console.log(e);
-  btn.classList.toggle("loading");
-  btn.innerHTML === "SEND"
-    ? (btn.innerHTML = "LOADING")
-    : (btn.innerHTML = "SEND");
+    console.log(e);
+    btn.classList.toggle("loading");
+    btn.innerHTML === "SEND" ? (btn.innerHTML = "LOADING") : (btn.innerHTML = "SEND");
+});
+
+// POST the data in the Google Spreadsheet
+const form = document.getElementById("sheetdb-form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(document.getElementById("sheetdb-form"));
+    const selectedValues = {};
+
+    // Loop over de checkboxen en voeg de geselecteerde waarden toe aan een object
+    document.querySelectorAll('input[type="checkbox"]:checked').forEach((checkbox) => {
+        const name = checkbox.name;
+        const value = checkbox.value;
+        if (!selectedValues[name]) {
+            selectedValues[name] = [];
+        }
+        selectedValues[name].push(value);
+    });
+
+    // Loop over het object met geselecteerde waarden en voeg ze toe aan de FormData
+    for (const name in selectedValues) {
+        formData.append(name, selectedValues[name].join(","));
+    }
+
+    fetch(form.action, {
+        method: "POST",
+        body: formData,
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            // Verwerk de response van SheetDB hier
+            console.log(data);
+            window.open("page2.html", "_blank");
+        });
 });
--- server.js
+++ server.js
@@ -67,38 +67,3 @@ async function fetchJson(url) {
 server.listen(server.get("port"), () => {
     console.log(`Application started on http://localhost:${server.get("port")}`);
 });
-
-// POST the data in the Google Spreadsheet
-const form = document.getElementById("sheetdb-form");
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const formData = new FormData(document.getElementById("sheetdb-form"));
-    const selectedValues = {};
-
-    // Loop over de checkboxen en voeg de geselecteerde waarden toe aan een object
-    document.querySelectorAll('input[type="checkbox"]:checked').forEach((checkbox) => {
-        const name = checkbox.name;
-        const value = checkbox.value;
-        if (!selectedValues[name]) {
-            selectedValues[name] = [];
-        }
-        selectedValues[name].push(value);
-    });
-
-    // Loop over het object met geselecteerde waarden en voeg ze toe aan de FormData
-    for (const name in selectedValues) {
-        formData.append(name, selectedValues[name].join(","));
-    }
-
-    fetch(form.action, {
-        method: "POST",
-        body: formData,
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            // Verwerk de response van SheetDB hier
-            console.log(data);
-            window.open("/overview.ejs", "_blank");
-        });
-});
